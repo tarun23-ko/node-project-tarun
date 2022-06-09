8664d48f9a707190bb08d5409c66fbe1049db0d8
Tarun Adhikary <bubaiadh@gmail.com>
2022-06-09 21:42:39 +0530

    Model stating and Added DB Connections

---
+++ backend/config/database.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const ConnectDatabse=()=>{
+mongoose.connect("mongodb://127.0.0.1:27017/Ecommerce",
+
+{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    
+    
+}).then((data) => {
+
+    console.log(data) 
+    console.log("Database Conneted")
+}).catch((err) => {
+    console.log(err)
+});
+}
+module.exports=ConnectDatabse;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const app = require('./app')
+const connect_DB = require('./config/database')
 const dotenv = require('dotenv')
 dotenv.config({path:'backend/config/config.env'})
+//Connect With Databse
+connect_DB()
 app.listen(3000,()=>{
 console.log( `Server is working at http://localhost:${process.env.PORT}`);
 })
